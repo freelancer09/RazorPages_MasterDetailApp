38dc37a3f7e3dde0aa3f9bb62968b8a23b6feb5d
freelancer09 <2009.mike@gmail.com>
2022-10-16 20:10:19 -0400

    fix

--- Models/SCP.cs
+++ Models/SCP.cs
@@ -13,21 +13,23 @@ namespace PDMasterDetail.Models
 
         [Display(Name = "Object Class")]
         [Required]
-        [StringLength(50)]
+        [StringLength(30)]
         public string ObjectClass { get; set; } = string.Empty;
 
+
         [Required]
-        [StringLength(50)]
+        [StringLength(60)]
         public string Classification { get; set; } = string.Empty;
-              
+
+
         [Required]
         [StringLength(1500, MinimumLength = 100)]
         public string Description { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(500)]
-        public string Containment { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(100)]
+        public string Containment { get; set; } = string.Empty;
         public string ImageName { get; set; } = string.Empty;
 
     }
